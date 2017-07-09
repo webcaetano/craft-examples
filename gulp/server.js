39f26862e92a15e94a31d7eb44b772adc00718c1
Andre Caetano <webandrecaetano@gmail.com>
2017-07-09 16:05:38 -0300

    accept craft webpack

--- gulp/server.js
+++ gulp/server.js
@@ -22,7 +22,7 @@ module.exports = function(options) {
 	gulp.task('serve', gulp.series('watch', browserSyncInit.bind(null,[
 		options.tmp,
 		'.',
-		'./../craft/.tmp',
+		'./../craft/build',
 		'src'
 	],null)));
 
